ebe3ea283f6ac6ac1003e2b790cf915dbf51eee3
Sudyko <63749891+Sudyko@users.noreply.github.com>
2021-03-30 18:03:59 +0300

    Update 1613.cpp

--- Timus/1613.cpp
+++ Timus/1613.cpp
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <vector>
-#include <utility>
 #include <algorithm>
 
 using namespace std;
@@ -8,25 +7,23 @@ using namespace std;
 vector<pair<int, int>> cities;
 
 int comp(pair<int, int> a, pair<int, int> b) {
-    if (a.first == b.first)
-        return a.second < b.second;
+    if (a.first == b.first) return a.second < b.second;
     return a.first < b.first;
 }
 
 int main() {
     int n;
-    scanf_s("%d", &n);
-    for (int i = 0; i < n; ++i) {
+    scanf("%d", &n);
+    for (size_t i = 0; i < n; ++i) {
         int num;
-        scanf_s("%d", &num);
+        scanf("%d", &num);
         cities.push_back(make_pair(num, i + 1));
     }
     sort(cities.begin(), cities.end());
-    
-    scanf_s("%d", &n);
-    for (int i = 0; i < n; ++i) {
+    scanf("%d", &n);
+    for (size_t i = 0; i < n; ++i) {
         int l, r, x;
-        scanf_s("%d %d %d", &l, &r, &x);
+        scanf("%d %d %d", &l, &r, &x);
         int left_bound = lower_bound(cities.begin(), cities.end(), make_pair(x, l), comp) - cities.begin();
         int right_bound = upper_bound(cities.begin(), cities.end(), make_pair(x, r), comp) - cities.begin();
         printf("%d", left_bound < right_bound);
